a7f9ba3a9ee9d5ca4959559d45451a408e59a8d3
New Author <new@email.com>
2017-01-07 01:10:08 +0300

    new comment

---
+++ MyApplication/app/src/main/java/com/julia_sk/favorite/myapplication/MainActivity.java
@@ -0,0 +1,13 @@
+package com.julia_sk.favorite.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
